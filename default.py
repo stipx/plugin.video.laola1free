c3787bd1bd0decb4c7e8d2f90dfaa6d20418c782
stipx <admin@stipx.com>
2016-03-04 15:50:15 +0100

    Add debug log option in addon scope

--- default.py
+++ default.py
@@ -5,18 +5,22 @@ import resources.lib.logger
 import urlparse
 import xbmcaddon
 
+addonhandle = int(sys.argv[1])
+addonbaseurl = sys.argv[0]
+
+addonname = addonbaseurl[9:-1]
+addon = xbmcaddon.Addon(id=addonname)
+
+logger.addon_debug_enabled = addon.getSetting('debug') == 'true'
+logger.notice('Debug logging enabled: ' + str(logger.addon_debug_enabled))
+
 logger.info('Starting addon with {}', sys.argv)
 try:
 	logger.info('Python version: {}.{}.{}', sys.version_info[0], sys.version_info[1], sys.version_info[2])
 except:
 	logger.warn('Python version info could not be loaded')
 
-addonhandle = int(sys.argv[1])
-addonbaseurl = sys.argv[0]
 parameters = urlparse.parse_qs(sys.argv[2][1:])
-
-addonname = addonbaseurl[9:-1]
-addon = xbmcaddon.Addon(id=addonname)
 
 languages = ['en', 'de']
 locations = ['int', 'de', 'at']
